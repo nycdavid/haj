341e2f215c7dfc6cfbbad636b419f81c33222af4
David Ko <davidsj.ko@gmail.com>
2022-11-08 08:27:47 -0500

    Ruby additions

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,6 +1,7 @@
 package lexer
 
 import (
+	"fmt"
 	"token"
 )
 
@@ -57,6 +58,13 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tokt := "foo"
+			fmt.Println(tokt)
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
@@ -65,4 +73,10 @@ func (l *Lexer) NextToken() token.Token {
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' ||
+		'A' <= ch && ch <= 'Z' ||
+		ch == '_'
 }
