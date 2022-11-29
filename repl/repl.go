96d6b6ff0640c4f52c33eff4b454a57484b9c167
David Ko <davidsj.ko@gmail.com>
2022-11-29 08:29:52 -0500

    Adds the repl package

---
+++ repl/repl.go
@@ -0,0 +1,32 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"go/token"
+	"io"
+
+	"lexer"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Pritnf("%+v\n", tok)
+		}
+	}
+}
