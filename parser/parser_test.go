032d71b4e2836aa6787b39eaf1ecc20d42bd56c1
David Ko <davidsj.ko@gmail.com>
2022-12-07 09:40:12 -0500

    Initial failing test for parser

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,1 +1,65 @@
 package parser
+
+import (
+	"haj/ast"
+	"testing"
+
+	"haj/lexer"
+)
+
+func TestAssignStatements(t *testing.T) {
+	input := `
+x = 5
+y = 10
+foobar = 838383
+`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf(
+			"program.Statements does not contain 3 statements. got=%d",
+			len(program.Statements),
+		)
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testAssignStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testAssignStatement(t *testing.T, s ast.Statement, name string) bool {
+	assignStmt, ok := s.(*ast.AssignStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatement. got=%T", s)
+		return false
+	}
+
+	if assignStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, assignStmt.Name.Value)
+		return false
+	}
+
+	if assignStmt.Name.TokenLiteral() != name {
+		t.Errorf("s.Name not '%s'. got=%s", name, assignStmt.Name)
+		return false
+	}
+
+	return true
+}
